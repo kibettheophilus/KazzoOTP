61c37bf5d8b4c976da852a949476c1059d3ab0ff
kibet <kibettheophilus@gmail.com>
2021-09-28 02:22:29 +0300

    First Commit

--- kazzootp/src/main/java/dev/kibet/kazzootp/constants/Resource.kt
+++
@@ -1,6 +0,0 @@
-package dev.kibet.kazzootp.constants
-
-sealed class Resource<out T> {
-    data class Success<out T>(val value: T) : Resource<T>()
-    data class Failure(val errorString: String): Resource<Nothing>()
-}
